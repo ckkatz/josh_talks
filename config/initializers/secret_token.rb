2543aa55d1ae89de803d39fcd10ac11317e69630
Charles K. Katz <ckkatz2003@yahoo.com>
2013-07-16 19:31:03 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoshTalks::Application.config.secret_token = 'cff8b007f2298db43afeacd11737b97d96fb70f601ea93d7a7d4b9cf1cef102e59851c5d75ae68eb174706d9ddebdb2cef29ee65cf825d078f493068734eaa26'
